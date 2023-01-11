31937a806c0aaa542d2b04001faa6a9e50e1ba66
Throre <pawtet100500@gmail.com>
2023-01-11 14:08:31 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    
+    <AddTodo 
+      @add-todo="addTodo"
+    />
+
+    <TodoList 
+    v-bind:todos="todos"
+    @remove-todo="removeTodo"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from '@/components/TodoList'
+import AddTodo from '@/components/AddTodo'
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  methods: {
+    removeTodo(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    },
+    addTodo(todo) {
+      this.todos.push(todo)
+    }
+  },
+  components: {
+    TodoList,
+    AddTodo
+  }
+}
+</script>
+
+<style>
+
+#app {
+  height: 700px;
+  font-family: 'Roboto', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  background-image: url("https://img.freepik.com/free-vector/quote-background-design_1257-196.jpg?w=2000");
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: 700px;
+}
+</style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,56 @@
+<template>
+  <form class="add-todo" @submit.prevent="onSubmit">
+    <input type="text" v-model="title" placeholder="Add what you need to do">
+    <button class="create" type="submit">Create</button>
+  </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            title: ''
+        }
+    },
+    methods: {
+        onSubmit() {
+            if(this.title.trim()) {
+                const newTodo = {
+                    id: Date.now(),
+                    title: this.title,
+                    completed: false
+                }
+                this.$emit('add-todo', newTodo)
+                this.title = ""
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .add-todo {
+        display: flex;
+        justify-content: center;
+        margin: 30px 0 30px 0;
+        padding: 30px;
+        box-sizing: border-box;
+    }
+    input {
+        width: 500px;
+        margin-right: 30px;
+        padding-left: 15px;
+        box-sizing: border-box;
+        border-radius: 5px;
+    }
+    .create {
+        width: 100px;
+        height: 30px;
+        background: rgb(113, 255, 100);
+        border-radius: 5px;
+        border: 1px solid grey;
+        font-size: 15px;
+        font-family: 'Roboto', sans-serif;
+        color: black;
+    }
+</style>
